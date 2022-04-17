f489f8cdc4374e6318ecf6557146032f177be25f
Code Sandbox <hello@codesandbox.io>
2022-04-17 11:19:59 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.b01fa80c2e668d5e13f92336b37473c0.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "931a62a39feb60fe91d9113813d816b8",
+    "url": "https://babelmandab.github.io/csb-kpsjon/index.html"
+  },
+  {
+    "revision": "d478e7f699f8a4fe0e4f",
+    "url": "https://babelmandab.github.io/csb-kpsjon/static/js/2.fc726699.chunk.js"
+  },
+  {
+    "revision": "b5a684ee89ac453589a3",
+    "url": "https://babelmandab.github.io/csb-kpsjon/static/js/main.4169133f.chunk.js"
+  },
+  {
+    "revision": "b5c1d869a208bb781e7e",
+    "url": "https://babelmandab.github.io/csb-kpsjon/static/js/runtime-main.de8c222c.js"
+  }
+]);
